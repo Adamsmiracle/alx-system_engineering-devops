f245af008ce097347702b020b6bf6447d71cb86a
adams miracle <adamsmiracle0@gmail.com>
2024-03-04 16:12:21 +0000

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
-# setup ubuntu web server using nginx
+# Setup New Ubuntu server with nginx
 
 exec { 'update system':
-		command => '/usr/bin/apt-get update',
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-		ensure => 'installed'
-		require => Exec ['update system']
-
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-		conten => 'Hello World!'
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-exec { 'redirect_me':
-		command => 'sed -i "24i\	rewrite ^/redirect_me https://www.google.com permanent;" /etc/nginx/sites-available/default',
-		provider => 'shell'
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 service {'nginx':
